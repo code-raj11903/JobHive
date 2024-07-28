2b5bbb678d943c07dc9b36b9d7c64953e07367ab
Raj Gupta <code.raj11903@gmail.com>
2024-07-28 16:31:35 +0530

    backend deployed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "job-hive-backend-pew3clezk-raj-guptas-projects-45cc88a4.vercel.app/api/v1/user/getuser",
+          "/api/v1/user/getuser",
           { withCredentials: true }
         );
         setUser(response.data.user);
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+  //get rid of the cors error
+  proxy: {
+    "/api":{
+      target : "https://jobhive-wpde.onrender.com",
+      changeOrigin: true,
+      secure:false,
+    }
+  }
+  }
 })
