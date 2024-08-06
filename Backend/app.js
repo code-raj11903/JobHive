03ee69443da392add3e80171dff160a37e1589a2
Raj Gupta <code.raj11903@gmail.com>
2024-08-06 16:03:41 +0530

    issue resolve path name

--- Backend/app.js
+++ Backend/app.js
@@ -44,11 +44,11 @@ dbConnection();
 // http://localhost:4000 => backend,frontend
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 	// react app
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
 	});
 }
 
